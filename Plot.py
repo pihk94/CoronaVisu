8050debd4c18c0dae0183b3862c922b2439999ec
pihk94 <43264132+pihk94@users.noreply.github.com>
2020-03-26 18:03:06 +0100

    add

--- Plot.py
+++ Plot.py
@@ -1,6 +1,7 @@
 import GetData
 import plotly.express as px
 from plotly.offline import plot
+import pandas as pd
 
 COL_REGION = 'Country/Region'
 
@@ -17,7 +18,14 @@ df_tmp = px.data.gapminder().query("year==2007")
 df_tmp.drop(['year', 'lifeExp', 'pop', 'gdpPercap', 'iso_num'], axis = 1, inplace = True)
 
 df_final = df_cases_world_group.merge(df_tmp, on = 'country')
-
+lst=[]
+for pays in df_final.country.unique():
+    for col in df_final.columns[1:-2]:
+        nb =df_final.set_index('country')[col][pays]
+        continent = df_final.set_index('country')['continent'][pays]
+        iso = df_final.set_index('country')['iso_alpha'][pays]
+        lst+= [(pays,col,nb,continent,iso)]
+df_ = pd.DataFrame(lst,columns=['Country','Date','Nombre','continent','iso_alpha'])
 #fig = px.scatter_geo(df_final, locations="iso_alpha", color="continent",
 #                     hover_name="country", size="Confirmed",
 #                     projection="natural earth")
