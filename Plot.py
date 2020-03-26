22360fde0f9cc3941a52db6939a1d670f4e085a0
YannickLy <62262190+YannickLy@users.noreply.github.com>
2020-03-26 19:03:29 +0100

    v4

--- Plot.py
+++ Plot.py
@@ -1,6 +1,9 @@
 import GetData
+
 import plotly.express as px
+import plotly.graph_objects as go
 from plotly.offline import plot
+
 import pandas as pd
 
 COL_REGION = 'Country/Region'
@@ -15,16 +18,24 @@ df_tmp = px.data.gapminder().query("year==2007")
 df_tmp.drop(['year', 'lifeExp', 'pop', 'gdpPercap', 'iso_num'], axis = 1, inplace = True)
 
 df_final = df_cases_world_group.merge(df_tmp, on = 'country')
-lst=[]
+lst = []
 for pays in df_final.country.unique():
     for col in df_final.columns[1:-2]:
-        nb =df_final.set_index('country')[col][pays]
+        nb = df_final.set_index('country')[col][pays]
         continent = df_final.set_index('country')['continent'][pays]
         iso = df_final.set_index('country')['iso_alpha'][pays]
         lst+= [(pays,col,nb,continent,iso)]
-df_ = pd.DataFrame(lst,columns=['Country','Date','Nombre','continent','iso_alpha'])
-#fig = px.scatter_geo(df_final, locations="iso_alpha", color="continent",
-#                     hover_name="country", size="Confirmed",
-#                     projection="natural earth")
-#plot(fig)
+df_ = pd.DataFrame(lst, columns=['country', 'date', 'nombre', 'continent', 'iso_alpha'])
+df_.replace(-1, 0, inplace = True)
 
+
+fig_point = go.Figure(go.Scattergeo(
+        ))
+
+
+
+fig_point?update_layout = px.scatter_geo(df_, locations = "iso_alpha", color = "continent", hover_name = "country", size = "nombre", animation_frame = 'date')
+plot(fig_point)
+
+#fig_color = px.choropleth(df_, locations = "iso_alpha", color = "nombre", hover_name = "country", animation_frame = 'date', range_color = [0, max(df_.nombre)])
+#plot(fig_color)
