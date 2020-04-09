0235ddd61153497148a269de0818acf9b2f3c6bb
pihk94 <43264132+pihk94@users.noreply.github.com>
2020-04-09 21:28:58 +0200

    add a lot

--- CoronaRecap/apps/evolution.py
+++ CoronaRecap/apps/evolution.py
@@ -97,14 +97,15 @@ fig4.update_layout(annotations=[dict(xref='paper', yref='paper', x=0.5, y=-0.2,
                               showarrow=False)])
 
 sidebar = html.Div(id='mySidebar',className ="sidebar",children=[
-            html.A(href='/',children = [html.Img(src='assets/png/home.png',style={'width':'32px','height':'32px','margin-left':'3.6em'}),html.Div('Home',style={
+            html.A(href='/',children = [html.Img(src='../assets/png/home.png',style={'width':'32px','height':'32px','margin-left':'5.6em'}),html.Div('Home',style={
                 'font-size':'14px',
                 'text-align':'center'
-            })],style={'background-color':'#036'}),
-            html.A(href='#',children='Récapitulatif',style={'text-align':'left'}),
-            html.A(href='#',children='Autres épidémies',style={'text-align':'left'}),
-            html.A(href='#',children='Finance',style={'text-align':'left'}),
-            html.A(href='#',children='GoogleTrend',style={'text-align':'left'}),
+            })],style={'background-color':'#036','margin-top':' 0px'}),
+            html.A(href='/recap',children=[html.Span('Summary')],style={'text-align':'left'}),
+            html.A(href='/simulation',children='SIMULATION',style={'text-align':'left'}),
+            html.A(href='/maladies',children='DISEASE COMPARISON',style={'text-align':'left'}),
+            html.A(href='/finance',children='FINANCE',style={'text-align':'left'}),
+            html.A(href='/GoogleTrend',children='INTERNET',style={'text-align':'left'}),
         ])
 
 layout = html.Div([
--- CoronaRecap/apps/world.py
+++ CoronaRecap/apps/world.py
@@ -15,15 +15,17 @@ import plotly.graph_objects as go
 
 #FCT 
 sidebar = html.Div(id='mySidebar',className ="sidebar",children=[
-            html.A(href='/',children = [html.Img(src='assets/png/home.png',style={'width':'32px','height':'32px','margin-left':'3.6em'}),html.Div('Home',style={
+            html.A(href='/',children = [html.Img(src='../assets/png/home.png',style={'width':'32px','height':'32px','margin-left':'5.6em'}),html.Div('Home',style={
                 'font-size':'14px',
                 'text-align':'center'
-            })],style={'background-color':'#036'}),
-            html.A(href='#',children='Récapitulatif',style={'text-align':'left'}),
-            html.A(href='#',children='Autres épidémies',style={'text-align':'left'}),
-            html.A(href='#',children='Finance',style={'text-align':'left'}),
-            html.A(href='#',children='GoogleTrend',style={'text-align':'left'}),
+            })],style={'background-color':'#036','margin-top':' 0px'}),
+            html.A(href='/recap',children=[html.Span('Summary')],style={'text-align':'left'}),
+            html.A(href='/simulation',children='SIMULATION',style={'text-align':'left'}),
+            html.A(href='/maladies',children='DISEASE COMPARISON',style={'text-align':'left'}),
+            html.A(href='/finance',children='FINANCE',style={'text-align':'left'}),
+            html.A(href='/GoogleTrend',children='INTERNET',style={'text-align':'left'}),
         ])
+
 
 layout = html.Div([
     sidebar,
