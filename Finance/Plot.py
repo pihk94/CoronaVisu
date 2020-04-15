322f5fa67f3368bee64973f1555f50f398a03017
YannickLy <62262190+YannickLy@users.noreply.github.com>
2020-04-15 11:02:26 +0200

    change

--- Finance/Plot.py
+++ Finance/Plot.py
@@ -158,15 +158,36 @@ plot(fig_impact_deaths_finance, filename = 'Finance/impact_deaths.html')
 
 ### Stock Market Returns, 2020 vs. Past Recessions
 
+from plotly.subplots import make_subplots
+fig = make_subplots(rows = 1, cols = 2, subplot_titles = ('Internet crisis (2001)', 'Subprime crisis (2008)'))
+
 data = yf.download('^DJI')['Adj Close']
-
 ret_now = data.loc['2020-01-30':].pct_change()[1:]
-ret_2007_2009 = data.loc['2007-01-03':'2009-12-31'].pct_change()[1:]
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(x = [1:754], y = ret_now, mode = 'lines', name = 'ret'))
-fig.add_trace(go.Scatter(x = [1:754], y = ret_now, mode = 'lines', name = 'ret'))
-plot(fig)
+# 2007_209 
+ret_2007_2009 = data.loc['2007-01-03':'2009-12-31'].pct_change()[1:]
+cum_ret_2007_2009 = np.cumprod(1 + ret_2007_2009) - 1
+ret_now_tmp = pd.DataFrame()
+ret_now_tmp['ret'] = range(len(ret_2007_2009))
+ret_now_tmp['ret'][0:51] = ret_now
+ret_now_tmp['ret'][51:] = np.nan
+cum_ret_now = np.cumprod(1 + ret_now_tmp) - 1
+
+fig.add_trace(go.Scatter(x = ret_2007_2009.index, y = cum_ret_2007_2009, mode = 'lines', name = 'ret_2007_2009'), row = 1, col = 2)
+fig.add_trace(go.Scatter(x = ret_2007_2009.index, y = cum_ret_now['ret'], mode = 'lines', name = 'ret_now'), row = 1, col = 2)
+
+# Bulle internet (Mars - Nov 2001)
+ret_2001 = data.loc['2001-03-01':'2001-11-30'].pct_change()[1:]
+cum_ret_ret_2001 = np.cumprod(1 + ret_2001) - 1
+ret_now_tmp = pd.DataFrame()
+ret_now_tmp['ret'] = range(len(ret_2001))
+ret_now_tmp['ret'][0:51] = ret_now
+ret_now_tmp['ret'][51:] = np.nan
+cum_ret_now = np.cumprod(1 + ret_now_tmp) - 1
+
+fig.add_trace(go.Scatter(x = ret_2001.index, y = cum_ret_ret_2001, mode = 'lines', name = 'ret_2007_2009'), row = 1, col = 1)
+fig.add_trace(go.Scatter(x = ret_2001.index, y = cum_ret_now['ret'], mode = 'lines', name = 'ret_now'), row = 1, col = 1)
+plot(fig, filename = 'Finance/past recesseions.html')
 
 
 
